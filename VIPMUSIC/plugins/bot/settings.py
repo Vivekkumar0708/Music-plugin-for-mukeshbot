706578ea78965226445cbcf22d8c9f4cb26494d7
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-31 21:50:49 +0530

    Update settings.py

--- VIPMUSIC/plugins/bot/settings.py
+++ VIPMUSIC/plugins/bot/settings.py
@@ -99,11 +99,7 @@ async def settings_back_markup(client, CallbackQuery: CallbackQuery, _):
         await app.resolve_peer(OWNER_ID)
         OWNER = OWNER_ID
         buttons = private_panel(_)
-        return await CallbackQuery.edit_message_text(
-            _["start_2"].format(CallbackQuery.from_user.mention, app.mention),
-            reply_markup=InlineKeyboardMarkup(buttons),
-        )
-        await callback_query.edit_message_media(
+        return await callback_query.edit_message_media(
         media=InputMediaPhoto(
             media=START_IMG_URL,
 caption=_["start_2"].format(CallbackQuery.from_user.mention, app.mention),
